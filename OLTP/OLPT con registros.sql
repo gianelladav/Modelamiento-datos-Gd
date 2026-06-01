fb991482ac54142e4c9b7c44ce95961ad24a3982
gianelladav <86448707+gianelladav@users.noreply.github.com>
2026-06-01 02:12:48 -0500

    update

--- OLTP/OLPT con registros.sql
+++ OLTP/OLPT con registros.sql
@@ -146,3 +146,141 @@ VALUES
 ('70123456','Camila','Navarro',27,'Peruana','Lima','Magdalena','Marketing','Soltero',0,'987654330','camila@gmail.com');
 
 select * from CLIENTE;
+ALTER TABLE VENTA
+ADD monto_separacion DECIMAL(12,2) NULL;
+
+
+select * from VENTA;
+
+UPDATE VENTA
+SET monto_separacion = 5000
+WHERE id_venta IN (2,5,9,13,17);
+
+UPDATE UNIDADES
+SET estado_disponibilidad = 'Separado'
+WHERE id_unidad IN (2,5,9,13,17);
+
+INSERT INTO UNIDADES
+(id_proyecto,codigo_unidad,tipo_inmueble,piso,tipologia,area_m2,nro_habitaciones,nro_baños,es_flat,precio_lista,moneda,estado_disponibilidad)
+VALUES
+(1,'M105','Departamento',5,'2 Dorm',75.50,2,2,0,420000,'Soles','Disponible'),
+(1,'M106','Departamento',6,'3 Dorm',95.00,3,2,0,580000,'Soles','Vendido'),
+(1,'M107','Departamento',7,'2 Dorm',78.00,2,2,0,450000,'Soles','Separado'),
+(1,'M108','Departamento',8,'1 Dorm',55.00,1,1,0,350000,'Soles','Disponible'),
+(1,'M109','Estacionamiento',0,'Simple',15.00,0,0,0,45000,'Soles','Disponible'),
+(1,'M110','Deposito',0,'Deposito',8.00,0,0,0,18000,'Soles','Disponible'),
+(1,'M111','Departamento',9,'3 Dorm',110.00,3,3,1,690000,'Soles','Vendido'),
+(1,'M112','Departamento',10,'2 Dorm',82.00,2,2,0,470000,'Soles','Separado'),
+
+(2,'S205','Departamento',5,'2 Dorm',72.00,2,2,0,430000,'Soles','Disponible'),
+(2,'S206','Departamento',6,'3 Dorm',98.00,3,2,0,610000,'Soles','Vendido'),
+(2,'S207','Departamento',7,'1 Dorm',50.00,1,1,0,330000,'Soles','Disponible'),
+(2,'S208','Estacionamiento',0,'Simple',14.00,0,0,0,42000,'Soles','Disponible'),
+
+(3,'B305','Departamento',5,'2 Dorm',70.00,2,2,0,410000,'Soles','Disponible'),
+(3,'B306','Departamento',6,'3 Dorm',100.00,3,2,0,620000,'Soles','Vendido'),
+(3,'B307','Departamento',7,'Loft',65.00,1,1,0,390000,'Soles','Separado'),
+(3,'B308','Closet',0,'Closet',4.00,0,0,0,12000,'Soles','Disponible'),
+
+(4,'SG405','Departamento',5,'2 Dorm',76.00,2,2,0,440000,'Soles','Disponible'),
+(4,'SG406','Departamento',6,'3 Dorm',102.00,3,3,0,650000,'Soles','Vendido'),
+
+(5,'MP505','Departamento',5,'2 Dorm',74.00,2,2,0,425000,'Soles','Disponible'),
+(5,'MP506','Departamento',6,'3 Dorm',99.00,3,2,0,605000,'Soles','Separado');
+
+INSERT INTO UNIDADES
+(id_proyecto,codigo_unidad,tipo_inmueble,piso,tipologia,area_m2,nro_habitaciones,nro_baños,es_flat,precio_lista,moneda,estado_disponibilidad)
+VALUES
+(1,'M113','Departamento',11,'2 Dorm',80.00,2,2,0,465000,'Soles','Disponible'),
+(1,'M114','Departamento',12,'3 Dorm',105.00,3,2,0,675000,'Soles','Vendido'),
+(1,'M115','Departamento',13,'1 Dorm',52.00,1,1,0,340000,'Soles','Disponible'),
+(1,'M116','Estacionamiento',0,'Simple',15.00,0,0,0,45000,'Soles','Disponible'),
+(1,'M117','Deposito',0,'Deposito',7.50,0,0,0,17000,'Soles','Disponible'),
+(1,'M118','Departamento',14,'2 Dorm',78.00,2,2,0,455000,'Soles','Separado'),
+(1,'M119','Departamento',15,'3 Dorm',108.00,3,3,1,710000,'Soles','Vendido'),
+(1,'M120','Departamento',16,'2 Dorm',82.00,2,2,0,475000,'Soles','Disponible'),
+
+(2,'S209','Departamento',8,'2 Dorm',75.00,2,2,0,440000,'Soles','Disponible'),
+(2,'S210','Departamento',9,'3 Dorm',101.00,3,2,0,620000,'Soles','Vendido'),
+(2,'S211','Departamento',10,'1 Dorm',54.00,1,1,0,345000,'Soles','Disponible'),
+(2,'S212','Deposito',0,'Deposito',6.00,0,0,0,15000,'Soles','Disponible'),
+(2,'S213','Departamento',11,'2 Dorm',77.00,2,2,0,460000,'Soles','Separado'),
+(2,'S214','Departamento',12,'3 Dorm',103.00,3,3,0,640000,'Soles','Vendido'),
+
+(3,'B309','Departamento',8,'2 Dorm',72.00,2,2,0,425000,'Soles','Disponible'),
+(3,'B310','Departamento',9,'3 Dorm',104.00,3,2,0,630000,'Soles','Vendido'),
+(3,'B311','Departamento',10,'1 Dorm',56.00,1,1,0,355000,'Soles','Disponible'),
+(3,'B312','Departamento',11,'2 Dorm',79.00,2,2,0,470000,'Soles','Separado'),
+
+(4,'SG407','Departamento',7,'2 Dorm',78.00,2,2,0,450000,'Soles','Disponible'),
+(4,'SG408','Departamento',8,'3 Dorm',106.00,3,3,0,670000,'Soles','Vendido');
+
+INSERT INTO VENTA
+(id_unidad,id_cliente,id_agente,id_canal,fecha_venta,
+precio_venta,moneda,estado_venta,forma_pago,
+monto_comision,monto_separacion)
+VALUES
+
+(21,6,1,2,'2026-05-01',420000,'Soles','Vigente','Crédito hipotecario',12600,NULL),
+(22,7,2,3,'2026-05-03',580000,'Soles','Vigente','Contado',17400,NULL),
+(23,8,3,1,'2026-05-05',450000,'Soles','Vigente','Crédito directo',13500,5000),
+(24,9,4,5,'2026-05-06',350000,'Soles','Vigente','Contado',10500,NULL),
+(25,10,5,4,'2026-05-08',45000,'Soles','Vigente','Contado',1350,NULL),
+
+(26,6,1,1,'2026-05-10',18000,'Soles','Vigente','Contado',540,NULL),
+(27,7,2,2,'2026-05-12',690000,'Soles','Vigente','Crédito hipotecario',20700,NULL),
+(28,8,3,3,'2026-05-14',470000,'Soles','Vigente','Crédito directo',14100,5000),
+
+(29,9,4,4,'2026-05-16',430000,'Soles','Vigente','Crédito hipotecario',12900,NULL),
+(30,10,5,5,'2026-05-18',610000,'Soles','Vigente','Contado',18300,NULL),
+(31,6,1,2,'2026-05-20',330000,'Soles','Vigente','Contado',9900,NULL),
+(32,7,2,1,'2026-05-22',42000,'Soles','Vigente','Contado',1260,NULL),
+
+(33,8,3,3,'2026-05-24',410000,'Soles','Vigente','Crédito hipotecario',12300,NULL),
+(34,9,4,4,'2026-05-25',620000,'Soles','Vigente','Crédito directo',18600,NULL),
+(35,10,5,5,'2026-05-27',390000,'Soles','Vigente','Crédito directo',11700,5000),
+(36,6,1,1,'2026-05-28',12000,'Soles','Vigente','Contado',360,NULL),
+
+(37,7,2,2,'2026-05-29',440000,'Soles','Vigente','Crédito hipotecario',13200,NULL),
+(38,8,3,3,'2026-05-30',650000,'Soles','Vigente','Crédito hipotecario',19500,NULL),
+(39,9,4,4,'2026-05-31',425000,'Soles','Vigente','Contado',12750,NULL),
+(40,10,5,5,'2026-06-01',605000,'Soles','Vigente','Crédito directo',18150,5000);
+
+UPDATE UNIDADES
+SET estado_disponibilidad = 'Separado'
+WHERE id_unidad IN (23,28,35,40);
+
+UPDATE UNIDADES
+SET estado_disponibilidad = 'Vendido'
+WHERE id_unidad IN
+(21,22,24,25,26,27,29,30,31,32,33,34,36,37,38,39);
+
+UPDATE UNIDADES
+SET tipologia = 'Simple'
+WHERE tipo_inmueble = 'Estacionamiento'
+AND tipologia IS NULL;
+
+UPDATE UNIDADES
+SET tipologia = 'Deposito'
+WHERE tipo_inmueble = 'Deposito';
+
+UPDATE UNIDADES
+SET tipologia = 'Closet'
+WHERE tipo_inmueble = 'Closet';
+
+UPDATE UNIDADES
+SET tipologia = 'Flat'
+WHERE tipo_inmueble = 'Departamento'
+AND tipologia IN ('1 Dorm','2 Dorm','3 Dorm');
+
+ALTER TABLE UNIDADES
+ADD CONSTRAINT CK_UNIDADES_TIPOLOGIA
+CHECK (
+    (tipo_inmueble = 'Departamento' AND tipologia IN ('Flat','Duplex','Triplex','Loft'))
+    OR
+    (tipo_inmueble = 'Estacionamiento' AND tipologia IN ('Simple','Doble'))
+    OR
+    (tipo_inmueble = 'Deposito' AND tipologia = 'Deposito')
+    OR
+    (tipo_inmueble = 'Closet' AND tipologia = 'Closet')
+);
---
+++ OLTP/consultas de Transact sql.sql
@@ -0,0 +1,188 @@
+/* Mostrar todos los proyectos */
+SELECT *
+FROM PROYECTO;
+
+/* Mostrar todas las unidades disponibles */
+SELECT *
+FROM UNIDADES
+WHERE estado_disponibilidad = 'Disponible';
+
+/* Mostrar todas las unidades vendidas */
+SELECT *
+FROM UNIDADES
+WHERE estado_disponibilidad = 'Vendido';
+
+/* Mostrar los clientes de Lima */
+SELECT *
+FROM CLIENTE
+WHERE provincia = 'Lima';
+
+/* Mostrar las ventas realizadas */
+SELECT *
+FROM VENTA
+WHERE estado_venta = 'Vigente';
+
+/* Mostrar las unidades ordenadas por precio de lista */
+SELECT codigo_unidad,
+       tipo_inmueble,
+       precio_lista
+FROM UNIDADES
+ORDER BY precio_lista DESC;
+
+/* Mostrar los clientes con nacionalidad peruana */
+SELECT *
+FROM CLIENTE
+WHERE nacionalidad = 'Peruana';
+
+/* Mostrar ventas realizadas mediante crédito hipotecario */
+SELECT *
+FROM VENTA
+WHERE forma_pago = 'Crédito hipotecario';
+
+/* Mostrar las unidades pertenecientes a Miraflores Tower */
+SELECT u.*
+FROM UNIDADES u
+INNER JOIN PROYECTO p
+ON u.id_proyecto = p.id_proyecto
+WHERE p.nombre_proyecto = 'Miraflores Tower';
+
+
+/* Mostrar ventas con información del cliente */
+SELECT
+    v.id_venta,
+    c.nombres,
+    c.apellidos,
+    v.precio_venta,
+    v.fecha_venta
+FROM VENTA v
+INNER JOIN CLIENTE c
+ON v.id_cliente = c.id_cliente;
+
+/* Cantidad de unidades por proyecto */
+SELECT
+    p.nombre_proyecto,
+    COUNT(*) AS Total_Unidades
+FROM UNIDADES u
+INNER JOIN PROYECTO p
+ON u.id_proyecto = p.id_proyecto
+GROUP BY p.nombre_proyecto;
+
+/* Total vendido por proyecto */
+SELECT
+    p.nombre_proyecto,
+    SUM(CASE WHEN v.moneda = 'Soles'
+             THEN v.precio_venta ELSE 0 END) AS Total_Soles,
+    SUM(CASE WHEN v.moneda = 'Dolares'
+             THEN v.precio_venta ELSE 0 END) AS Total_Dolares
+FROM VENTA v
+INNER JOIN UNIDADES u
+    ON v.id_unidad = u.id_unidad
+INNER JOIN PROYECTO p
+    ON u.id_proyecto = p.id_proyecto
+GROUP BY p.nombre_proyecto;
+
+/* Porcentaje de unidades vendidas por proyecto */
+SELECT
+    p.nombre_proyecto,
+    CONCAT(
+        CAST(
+            ROUND(
+                COUNT(CASE WHEN u.estado_disponibilidad = 'Vendido' THEN 1 END) * 100.0
+                / COUNT(*),
+                0
+            ) AS INT
+        ),
+        '%'
+    ) AS Porcentaje_Vendido
+FROM UNIDADES u
+INNER JOIN PROYECTO p
+    ON u.id_proyecto = p.id_proyecto
+GROUP BY p.nombre_proyecto;
+
+
+SELECT
+    p.nombre_proyecto,
+    u.codigo_unidad,
+    u.estado_disponibilidad,
+    c.nombres + ' ' + c.apellidos AS Cliente,
+    a.nombres + ' ' + a.apellidos AS Agente,
+    cc.nombre_canal,
+    v.precio_venta,
+    v.monto_comision,
+    v.fecha_venta
+FROM VENTA v
+INNER JOIN UNIDADES u
+    ON v.id_unidad = u.id_unidad
+INNER JOIN PROYECTO p
+    ON u.id_proyecto = p.id_proyecto
+INNER JOIN CLIENTE c
+    ON v.id_cliente = c.id_cliente
+INNER JOIN AGENTE_VENTA a
+    ON v.id_agente = a.id_agente
+INNER JOIN CANAL_CAPTACION cc
+    ON v.id_canal = cc.id_canal
+ORDER BY v.fecha_venta DESC;
+
+SELECT DB_NAME() AS BaseActual;
+
+SELECT id_proyecto, nombre_proyecto
+FROM PROYECTO
+ORDER BY id_proyecto;
+
+SELECT
+    id_unidad,
+    codigo_unidad,
+    id_proyecto
+FROM UNIDADES
+ORDER BY id_unidad;
+
+SELECT COUNT(*) AS TotalUnidades
+FROM UNIDADES;
+
+SELECT COUNT(*) AS Totalventas
+FROM VENTA;
+
+SELECT
+    p.nombre_proyecto,
+    COUNT(*) AS Total_Unidades,
+    STRING_AGG(u.codigo_unidad, ', ') AS Codigos_Unidades
+FROM UNIDADES u
+INNER JOIN PROYECTO p
+    ON u.id_proyecto = p.id_proyecto
+GROUP BY p.nombre_proyecto;
+
+SELECT
+    id_unidad,
+    id_proyecto,
+    codigo_unidad,
+    tipo_inmueble,
+    piso
+FROM UNIDADES
+ORDER BY id_proyecto, id_unidad;
+
+SELECT name
+FROM sys.check_constraints
+WHERE parent_object_id = OBJECT_ID('UNIDADES');
+
+ALTER TABLE UNIDADES
+DROP CONSTRAINT CK__UNIDADES__piso__4F7CD00D;
+
+UPDATE UNIDADES
+SET piso = -1
+WHERE tipo_inmueble = 'Estacionamiento';
+
+UPDATE UNIDADES
+SET piso = -2
+WHERE tipo_inmueble = 'Deposito';
+
+UPDATE UNIDADES
+SET piso = -2
+WHERE tipo_inmueble = 'Closet';
+
+SELECT
+    tipo_inmueble,
+    piso,
+    COUNT(*) AS cantidad
+FROM UNIDADES
+GROUP BY tipo_inmueble, piso
+ORDER BY piso, tipo_inmueble;
